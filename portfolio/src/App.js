fce4ea39e1c75e19236dac7e448d10a81de0a407
Jaden <jadentr.44@gmail.com>
2022-08-29 13:23:42 -0500

    added nav

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,13 +1,16 @@
-
+import React, { useState } from 'react';
 import './App.css';
 import NAV from './components/NAV';
 import Body from './components/Body';
 import Footer from './components/Footer';
 
+
 function App() {
+  const [currentPage, setCurrentPage] = useState('Home');
+  const handlePageChange = (page) => setCurrentPage(page);
   return (
     <>
-    <NAV/>
+    <NAV currentPage={currentPage} handlePageChange={handlePageChange}/>
     <Body/>
     <Footer/>
     </>
