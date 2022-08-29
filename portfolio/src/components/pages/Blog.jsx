4480f45719bee4a165a90083fea3da7c840180e2
Jaden <jadentr.44@gmail.com>
2022-08-29 15:26:07 -0500

    added blog post

--- portfolio/src/components/pages/Blog.jsx
+++ portfolio/src/components/pages/Blog.jsx
@@ -2,6 +2,55 @@ import React from 'react'
 
 export default function Blog() {
   return (
-    <div>Blog</div>
+    <div className="row mx-5 px-5">
+      <div className="col-4">
+      <div className="card" style={{width: '18rem'}}>
+  <img className='card-img-top' src="https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Blog 1</h5>
+    <p className="card-text">Heres wherer a blog post would go</p>
+    
+  </div>
+</div>
+      </div>
+
+
+      <div className="col-4">
+      <div className="card" style={{width: '18rem'}}>
+  <img className='card-img-top' src="https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Blog 1</h5>
+    <p className="card-text">Heres wherer a blog post would go</p>
+    
+  </div>
+</div>
+      </div>
+
+     
+      <div className="col-4">
+      <div className="card" style={{width: '18rem'}}>
+  <img className='card-img-top' src="https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Blog 1</h5>
+    <p className="card-text">Heres wherer a blog post would go</p>
+    
+  </div>
+</div>
+      </div>
+
+
+      <div className="col-4">
+      <div className="card" style={{width: '18rem'}}>
+  <img className='card-img-top' src="https://mailrelay.com/wp-content/uploads/2018/03/que-es-un-blog-1.png" alt="" />
+  <div className="card-body">
+    <h5 className="card-title">Blog 1</h5>
+    <p className="card-text">Heres wherer a blog post would go</p>
+    
+  </div>
+</div>
+      </div>
+
+
+    </div>
   )
 }
