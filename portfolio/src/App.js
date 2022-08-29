4bd2a3cbf477be2ab89325bf7053c04aaa8ad90e
Jaden <jadentr.44@gmail.com>
2022-08-29 14:52:08 -0500

    footer is at the bottom

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,19 +1,36 @@
 import React, { useState } from 'react';
 import './App.css';
 import NAV from './components/NAV';
-import Body from './components/Body';
 import Footer from './components/Footer';
+import About from './components/pages/About'
+import Blog from './components/pages/Blog'
+import Contact from './components/pages/Contact'
+import Home from './components/pages/Home'
 
 
 function App() {
   const [currentPage, setCurrentPage] = useState('Home');
   const handlePageChange = (page) => setCurrentPage(page);
+
+  const renderPage = () => {
+    if (currentPage === 'Home') {
+      return <Home />;
+    }
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Blog') {
+      return <Blog />;
+    }
+    return <Contact />;
+  };
+
   return (
-    <>
+    <div className='vh-100'>
     <NAV currentPage={currentPage} handlePageChange={handlePageChange}/>
-    <Body/>
+    {renderPage()}
     <Footer/>
-    </>
+    </div>
   );
 }
 
