19cf1b07b0ee30cb2dbf5c8fbb1b488d51b5c1ca
Jaden <jadentr.44@gmail.com>
2022-08-29 13:28:23 -0500

    addd footer

--- portfolio/src/components/Footer.jsx
+++ portfolio/src/components/Footer.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer id="sticky-footer" className="flex-shrink-0 py-4 bg-dark text-white-50">
+    <div className="container text-center">
+      <small>Copyright &copy; Jaden Rodriguez</small>
+    </div>
+  </footer>
   )
 }
